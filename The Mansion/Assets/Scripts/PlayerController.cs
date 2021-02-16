2cb96ee3a180c5e28545d2105dd3c398e0c1f0c8
Adel <adelou001@hotmail.com>
2021-02-16 16:03:32 +0100

    Player movement improved, working camera and walk/run

--- The Mansion/Assets/Scripts/PlayerController.cs
+++ The Mansion/Assets/Scripts/PlayerController.cs
@@ -6,35 +6,57 @@ namespace TheMansion
 {
 public class PlayerController : MonoBehaviour
     {
+        private float stamina = 100f;
+        public Transform BasePosition;
+        public Transform WalkRight;
+        public Transform WalkLeft;
+        public Transform RunRight;
+        public Transform RunLeft;
+
         // Start is called before the first frame update
         void Start()
         {
-
+            
         }
 
         // Update is called once per frame
         void Update()
         {
+            Debug.Log(stamina);
+
             if (Input.touchCount > 0)
             {
                 Touch touch = Input.GetTouch(0);
                 Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
 
-                if (touchPosition.x > 0)
+                if (touchPosition.x > BasePosition.position.x && touchPosition.x <= WalkRight.position.x)
                 {
-                    Debug.Log("Right");
+                    Debug.Log("Walk Right");
                     transform.Translate((Vector3.right * Time.deltaTime) * 1f);
+                    Camera.main.transform.Translate((Vector3.right * Time.deltaTime) * 1f);
                 }
 
-                if (touchPosition.x < 0)
+                if (touchPosition.x < BasePosition.position.x && touchPosition.x >= WalkLeft.position.x)
                 {
-                    Debug.Log("Left");
+                    Debug.Log("Walk Left");
                     transform.Translate((Vector3.left * Time.deltaTime) * 1f);
+                    Camera.main.transform.Translate((Vector3.left * Time.deltaTime) * 1f);
                 }
 
-                if (touchPosition.x >= 10 || touchPosition.x <= -10)
+                if (touchPosition.x > WalkRight.position.x && touchPosition.x <= RunRight.position.x)
                 {
-                    Debug.Log("END");
+                    Debug.Log("Run Right");
+                    transform.Translate((Vector3.right * Time.deltaTime) * 5f);
+                    Camera.main.transform.Translate((Vector3.right * Time.deltaTime) * 5f);
+                    stamina = stamina - 1 * Time.deltaTime;
+                }
+
+                if (touchPosition.x < WalkLeft.position.x && touchPosition.x >= RunLeft.position.x)
+                {
+                    Debug.Log("Run Left");
+                    transform.Translate((Vector3.left * Time.deltaTime) * 5f);
+                    Camera.main.transform.Translate((Vector3.left * Time.deltaTime) * 5f);
+                    stamina = stamina - 1 * Time.deltaTime;
                 }
             }
         }
