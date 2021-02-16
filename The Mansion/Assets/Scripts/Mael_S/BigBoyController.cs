90b54eaa15e2d4b7e2d021ff4c42ed3eadb2d3ee
maelRicou <m.ricou@rubika-edu.com>
2021-02-16 08:34:27 +0000

    Tweaks

--- The Mansion/Assets/Scripts/Mael_S/BigBoyController.cs
+++ The Mansion/Assets/Scripts/Mael_S/BigBoyController.cs
@@ -2,49 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BigBoyController : MonoBehaviour
+
+namespace TheMansion
 {
-    public float bigBoySpeed;
 
-    public Transform[] moveSpots;
-    private int randomSpot;
 
-    public bool isPatrolling;
-    [SerializeField] float waitTime;
-    float startWaitTime;
-
-    private void Start()
+    public class BigBoyController : MonoBehaviour
     {
-        isPatrolling = true;
+        public float bigBoySpeed;
 
-        waitTime = startWaitTime;
-        randomSpot = Random.Range(0, moveSpots.Length);
-    }
+        public Transform[] moveSpots;
+        private int randomSpot;
 
-    private void Update()
-    {
-        if (isPatrolling)
+        public bool isPatrolling;
+        public bool isRunning;
+
+
+        [SerializeField] Transform target;
+
+        [SerializeField] float waitTime;
+        float startWaitTime;
+
+
+        GameObject player;
+
+        private void Start()
         {
-            BBMPA();
+            isPatrolling = true;
+
+
+            waitTime = startWaitTime;
+            randomSpot = Random.Range(0, moveSpots.Length);
         }
-    }
 
-
-    public void BBMPA()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, bigBoySpeed * Time.deltaTime);
-
-        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        private void Update()
         {
-            if (waitTime <= 0)
+            if (isPatrolling)
             {
-                randomSpot = Random.Range(0, moveSpots.Length);
-                waitTime = startWaitTime;
-            }
-            else
-            {
-                waitTime -= Time.deltaTime;
+                BBMPA();
             }
         }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.tag == "Player" && isPatrolling)
+            {
+                BBMPO();
+            }
+        }
+
+
+        public void BBMPA()
+        {
+            transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, bigBoySpeed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+            {
+                if (waitTime <= 0)
+                {
+                    randomSpot = Random.Range(0, moveSpots.Length);
+                    waitTime = startWaitTime;
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+            }
+        }
+
+        public void BBMPO()
+        {
+            isPatrolling = false;
+            isRunning = true;
+
+
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, target.position);
+        }
+
     }
 }
