07432857803ade4c20abaab7be4fc5154cdada7a
Adel <adelou001@hotmail.com>
2021-02-19 16:34:30 +0100

    Stamina fixed and Hiding spot improved

--- The Mansion/Assets/Scripts/PlayerController.cs
+++ The Mansion/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,9 @@ namespace TheMansion
 {
 public class PlayerController : MonoBehaviour
     {
-        private int stamina = 100;
+        private int stamina = 500;
 
+        private bool isMoving;
         private bool canInteract;
         private bool isHiding;
 
@@ -31,55 +32,70 @@ public class PlayerController : MonoBehaviour
                 Vector2 touchPosition2D = new Vector2(touchPosition.x, touchPosition.y);
 
                 if (Input.GetTouch(0).phase == touchPhase)
-                {
+                {   
                     RaycastHit2D hitInformation = Physics2D.Raycast(touchPosition2D, Camera.main.transform.forward);
 
-                    if (hitInformation.collider != null && hitInformation.collider.tag != "Player")
+                    if (hitInformation.collider != null)
                     {
                         //We should have hit something with a 2D Physics collider!
                         GameObject touchedObject = hitInformation.transform.gameObject;
                         //touchedObject should be the object someone touched.
-                        Debug.Log("Touched " + touchedObject.transform.name); ;
+                        Debug.Log("Touched " + touchedObject.transform.name);
 
-                        if (touchedObject.tag == "Hard Hiding Spot" && canInteract)
+                        if (isHiding)
                         {
-                            isHiding = true;
-                            Debug.Log("Is Hiding");                           
-                            transform.position = touchedObject.transform.position;
-                            StartCoroutine(Hiding());
+                            if (touchedObject.tag == "Hard Hiding Spot")
+                            {
+                                isHiding = false;
+                                gameObject.GetComponent<Collider2D>().enabled = true;
+                            }
                         }
+                        else
+                        {
+                            if (touchedObject.tag == "Hard Hiding Spot" && canInteract)
+                            {
+                                isHiding = true;
+                                Debug.Log("Is Hiding");
+                                transform.position = touchedObject.transform.position;
+                                StartCoroutine(Hiding());
+                            }
+                        }                                         
                     }
                 }
 
-                if (touchPosition.x > BasePosition.position.x && touchPosition.x <= WalkRight.position.x)
+                if (touchPosition.x > BasePosition.position.x && touchPosition.x <= WalkRight.position.x && !isHiding)
                 {
                     Debug.Log("Walk Right");
+                    isMoving = true;
                     transform.Translate((Vector3.right * Time.deltaTime) * 1f);
                 }
 
-                if (touchPosition.x < BasePosition.position.x && touchPosition.x >= WalkLeft.position.x)
+                if (touchPosition.x < BasePosition.position.x && touchPosition.x >= WalkLeft.position.x && !isHiding)
                 {
                     Debug.Log("Walk Left");
+                    isMoving = true;
                     transform.Translate((Vector3.left * Time.deltaTime) * 1f);
                 }
 
-                if (touchPosition.x > WalkRight.position.x && touchPosition.x <= RunRight.position.x)
+                if (touchPosition.x > WalkRight.position.x && touchPosition.x <= RunRight.position.x && stamina > 0 && !isHiding) 
                 {
                     Debug.Log("Run Right");
+                    isMoving = true;
                     transform.Translate((Vector3.right * Time.deltaTime) * 5f);
                     StartCoroutine(StaminaLoss());
                 }
 
-                if (touchPosition.x < WalkLeft.position.x && touchPosition.x >= RunLeft.position.x)
+                if (touchPosition.x < WalkLeft.position.x && touchPosition.x >= RunLeft.position.x && stamina > 0 && !isHiding)
                 {
                     Debug.Log("Run Left");
+                    isMoving = true;
                     transform.Translate((Vector3.left * Time.deltaTime) * 5f);
                     StartCoroutine(StaminaLoss());                   
                 }
             }
         }
 
-        void OnTriggerStay2D(Collider2D InteractableObject)
+        void OnTriggerEnter2D(Collider2D InteractableObject)
         {
             if (InteractableObject.tag == "Hard Hiding Spot")
             {
@@ -87,6 +103,11 @@ public class PlayerController : MonoBehaviour
                 canInteract = true;
             }
         }
+        void OnTriggerExit2D(Collider2D InteractableObject)
+        {
+            canInteract = false;
+        }
+
 
         IEnumerator StaminaLoss()
         {
