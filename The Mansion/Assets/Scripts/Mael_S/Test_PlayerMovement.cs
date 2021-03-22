ede69a7d5338b668e9e639060ae98865eef912ac
maelRicou <m.ricou@rubika-edu.com>
2021-03-22 15:59:31 +0000

    Intégration niveau jouable 2/2 + Jouabilité prog 1/2

---
+++ The Mansion/Assets/Scripts/Mael_S/Test_PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private float inputHorizontal;
+    private float inputVertical;
+
+    Rigidbody2D rb;
+
+    public float distance;
+    public LayerMask whatIsLadder;
+
+    private bool isClimbing;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsLadder);
+
+
+        if(hitInfo.collider != null)
+        {
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                isClimbing = true;
+            }
+        }
+        else
+        {
+            isClimbing = false;
+        }
+
+        if (isClimbing)
+        {
+            inputVertical = Input.GetAxisRaw("Vertical");
+            rb.velocity = new Vector2(rb.velocity.x, inputVertical * speed);
+            rb.gravityScale = 0;
+        }
+        else
+        {
+            rb.gravityScale = 1;
+        }
+    }
+
+    /*private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, hit);
+    }*/
+
+}
