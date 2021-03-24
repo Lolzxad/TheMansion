dc935295d13346357cb57824eaa10ac7da4f06a2
maelRicou <m.ricou@rubika-edu.com>
2021-03-24 20:23:04 +0000

    Jouabilité prog (premier niveau jouable) 2/2

--- The Mansion/Assets/Scripts/Mael_S/Test_PlayerMovement.cs
+++ The Mansion/Assets/Scripts/Mael_S/Test_PlayerMovement.cs
@@ -10,15 +10,46 @@ public class Test_PlayerMovement : MonoBehaviour
     private float inputVertical;
 
     Rigidbody2D rb;
+    GameObject player;
 
-    public float distance;
-    public LayerMask whatIsLadder;
 
-    private bool isClimbing;
+    private enum State { climb}
+    
+
+    //Ladder Variable
+    public bool botLadder = false;
+    public bool topLadder = false;
+    public bool canClimb = false;
+    public Ladder ladder;
+    private float defaultGravity;
+    [SerializeField] float climbSpeed = 3f;
+
+   /* public float distance;
+    public LayerMask whatIsLadder;*/
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        defaultGravity = rb.gravityScale;
+    }
+
+    private void Update()
+    {
+        if(canClimb && Mathf.Abs(Input.GetAxis("Vertical")) > .1f)
+        {
+            Climb();
+            
+        }
+
+        if (!canClimb)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            //canClimb = false;
+            GetComponent<BoxCollider2D>().enabled = true;
+            rb.gravityScale = defaultGravity;
+
+            Physics2D.IgnoreLayerCollision(9, 10, false);
+        }
     }
 
 
@@ -27,10 +58,20 @@ public class Test_PlayerMovement : MonoBehaviour
         inputHorizontal = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y);
 
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsLadder);
+        
+
+        if (canClimb)
+        {
+            inputVertical = Input.GetAxisRaw("Vertical");
+            rb.velocity = new Vector2(rb.velocity.x, inputVertical * climbSpeed);
+
+            
+        }
+
+        // RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsLadder);
 
 
-        if(hitInfo.collider != null)
+        /*if(hitInfo.collider != null)
         {
 
             if (Input.GetKeyDown(KeyCode.UpArrow))
@@ -52,7 +93,34 @@ public class Test_PlayerMovement : MonoBehaviour
         else
         {
             rb.gravityScale = 1;
+        }*/
+    }
+
+    void Climb()
+    {
+        Physics2D.IgnoreLayerCollision(9, 10);
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+        transform.position = new Vector3(ladder.transform.position.x, rb.position.y);
+
+        //GetComponent<BoxCollider2D>().enabled = false;
+        rb.gravityScale = 0;
+
+
+        inputVertical = Input.GetAxis("Vertical");
+
+        if(inputVertical > .1f && !topLadder)
+        {
+            rb.velocity = new Vector2(0f, inputVertical * climbSpeed);
         }
+        else if (inputVertical < -.1f && !botLadder)
+        {
+            rb.velocity = new Vector2(0f, inputVertical * climbSpeed);
+        }
+        else
+        {
+
+        }
+
     }
 
     /*private void OnDrawGizmosSelected()
---
+++ The Mansion/Assets/Scripts/Mael_S/VictoryManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TheMansion
+{
+
+
+    public class VictoryManager : MonoBehaviour
+    {
+        GameObject player;
+
+        public bool canWin;
+
+        public bool sideObj1;
+        public bool sideObj2;
+
+        #region Levels
+        public bool isLevel1;
+        public bool isLevel2;
+        public bool isLevel3;
+        public bool isLevel4;
+        public bool isLevel5;
+        public bool isLevel6;
+        public bool isLevel7;
+
+        #endregion
+
+        private void Start()
+        {
+            //provisoire
+            isLevel1 = true;
+
+            player = GameObject.FindGameObjectWithTag("Player");
+            canWin = false;
+
+            sideObj1 = false;
+            sideObj2 = false;
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.name == "Player")
+            {
+                if (isLevel1)
+                {
+                    if (canWin && sideObj1)
+                    {
+                        Debug.Log("Level complete!");
+                    }
+
+                    if (!canWin)
+                    {
+                        Debug.Log("Quest item done");
+                        sideObj1 = true;
+                        canWin = true;
+                    }
+                }
+                
+
+            }
+        }
+    }
+}
