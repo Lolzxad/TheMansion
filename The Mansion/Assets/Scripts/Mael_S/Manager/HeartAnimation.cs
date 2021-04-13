4556a5a02903c86be136feee51217cddebd4bbc5
maelRicou <m.ricou@rubika-edu.com>
2021-04-13 17:35:36 +0100

    ALPHA - Premiere iteration
    3 niveaux, PlayerController, anim coeur selon heartbeat, pause script avec placeholder, scenes clean pour GA, camera smooth, bigboy rework, bugs de la Build 03/04 corriges

---
+++ The Mansion/Assets/Scripts/Mael_S/Manager/HeartAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheMansion
+{
+    public class HeartAnimation : MonoBehaviour
+    {
+        Animator anim;
+
+        PlayerController player;
+
+        private void Start()
+        {
+            anim = gameObject.GetComponent<Animator>();
+
+            player = FindObjectOfType<PlayerController>();
+        }
+
+        public void Update()
+        {
+            if(player.heartBeat <= 120)
+            {
+                anim.speed = 1;
+            }
+
+            if(player.heartBeat >= 120)
+            {
+                anim.speed = 2;
+            }
+
+            if(player.heartBeat >= 140)
+            {
+                anim.speed = 3;
+            }
+        }
+    }
+}
+
---
+++ The Mansion/Assets/Scripts/Mael_S/UI/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    GameObject player;
+
+    public GameObject pauseMenuUI;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+
+       
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+
+        
+    }
+
+    public void LoadMenu()
+    {
+        //SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
