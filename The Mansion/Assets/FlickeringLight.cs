b6b6ce9dd88c86c248bbc9dac3f3eb5934011bf4
Robin Seignolles <Robin.seignolles@hotmail.fr>
2021-05-30 19:25:44 +0200

    FixScript

---
+++ The Mansion/Assets/FlickeringLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+
+    public Animator animController;
+    public int indexAnim;
+    public float delayFlicker;
+
+    void Start()
+    {
+        
+    }
+
+    IEnumerator WaitTillFlicker()
+    {
+        yield return new WaitForSeconds(delayFlicker);
+    }
+
+}
